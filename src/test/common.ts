97b89e2f8adad81acedccbc45d16d030510f034d
Adam Morgan <atom.morgan@gmail.com>
2016-10-09 14:25:34 -0400

    Add promise support for chai-http

--- src/test/common.ts
+++ src/test/common.ts
@@ -3,11 +3,15 @@ process.env.NODE_ENV = 'test';
 import chai = require('chai');
 import chaiHttp = require('chai-http');
 import spies = require('chai-spies');
+import Promise = require('bluebird');
 
 global.chai = chai;
 global.expect = global.chai.expect;
 global.should = global.chai.should();
 global.chaiHttp = chaiHttp;
+global.Promise = Promise;
+
+console.log('promise ', global.Promise);
 
 chai.use(chaiHttp);
 chai.use(spies);
